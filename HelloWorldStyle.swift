edb5dd73168f8fb521ecc9f544efda64dd17b297
Zak Goneau <zak.goneau@stu.ocsb.ca>
2025-02-10 15:52:36 +0000

    Fixed pushing errors.

---
+++ HelloWorldStyle.swift
@@ -0,0 +1,12 @@
+//
+//  HelloWorld.swift
+//
+//  Created by Zak Goneau
+//  Created on 2025-02-06
+//  Version 1.0
+//  Copyright (c) 2025 Zak Goneau. All rights reserved.
+//
+//  Prints a custom message in Swift
+
+print("Hello, Ms Raffin")
+print("How is your day?")
